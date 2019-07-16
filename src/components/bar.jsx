05a6ffec2e8fad1c44c168ca7391e93343fc911c
BuildTools <unconfigured@null.spigotmc.org>
2019-07-16 15:40:38 +0200

    added todo message

--- src/components/bar.jsx
+++ src/components/bar.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+/*
+  Todo: Make this bar a progress bar on the blog side.
+        that determines shows how much longer in the post you've got.
+  Todo: Save on the backend the % of how far you got in componentDidUnmount or similar pageclosing script.
+*/
+
 const lineStyle = {
   border: "2px solid #6057E5",
   position: "sticky",
